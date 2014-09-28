374dcc7c235b38d453366223287eab60768d09fd
a1bin0 <si.h@aol.de>
2014-09-29 00:00:41 +0900

    6方向と停止を認識する、5秒以内に同じジェスチャーを2度認識しない

--- MainWindow.cs
+++ MainWindow.cs
@@ -8,27 +8,67 @@ using System.Reflection;
 using NITE;
 using OpenNI;
 using System.Diagnostics;
+using System.Collections.Generic;
 
 namespace SimpleViewer.net
 {
     public partial class MainWindow : Form
     {
+        private Context context;
+        private ScriptNode scriptNode;
+        private DepthGenerator depth;
+        private Thread readerThread;
+        private bool shouldRun;
+        private Bitmap bitmap;
+        private int[] histogram;
+        private SessionManager sessionManager;
+
+        enum ActionId
+        {
+            push,
+            stable,
+            down,
+            up,
+            right,
+            left,
+            steady,
+        }
+
+        private Dictionary<ActionId, DateTime> timeStamp = new Dictionary<ActionId, DateTime>();
+
+        static readonly int interval = 5;
+
         public MainWindow()
         {
             InitializeComponent();
-
-            this.context = Context.CreateFromXmlFile(SAMPLE_XML_FILE, out scriptNode);//SampleConfigのアドレス、変えます
-
-            this.sessionManager = new SessionManager(context, "Wave", "RaiseHand");
+            this.context = Context.CreateFromXmlFile(@"../../MotionCaptureAudio/Setting/Config.xml", out scriptNode);
+            this.sessionManager = new SessionManager(context, "Wave");
             sessionManager.SessionStart += new EventHandler<PositionEventArgs>(sessionManager_SessionStart);
+            var now = DateTime.Now.AddSeconds(-interval);
+
+            timeStamp[ActionId.push] = now;
+            timeStamp[ActionId.stable] = now;
+            timeStamp[ActionId.down] = now;
+            timeStamp[ActionId.up] = now;
+            timeStamp[ActionId.right] = now;
+            timeStamp[ActionId.left] = now;
+            timeStamp[ActionId.steady] = now;
 
             PushDetector push = new PushDetector();
             push.Push += new EventHandler<VelocityAngleEventArgs>(push_Push);
+            push.Stable += new EventHandler<VelocityEventArgs>(push_Stable);
             sessionManager.AddListener(push);
 
-            WaveDetector wave = new WaveDetector();
-            wave.Wave += new EventHandler(wave_Wave);
-            sessionManager.AddListener(wave);
+            SwipeDetector swipe = new SwipeDetector();
+            swipe.SwipeDown += new EventHandler<VelocityAngleEventArgs>(swipe_SwipeDown);
+            swipe.SwipeUp += new EventHandler<VelocityAngleEventArgs>(swipe_SwipeUp);
+            swipe.SwipeRight += new EventHandler<VelocityAngleEventArgs>(swipe_SwipeRight);
+            swipe.SwipeLeft += new EventHandler<VelocityAngleEventArgs>(swipe_SwipeLeft);
+            sessionManager.AddListener(swipe);
+
+            SteadyDetector steady = new SteadyDetector();
+            steady.Steady += new EventHandler<SteadyEventArgs>(steady_Steady);
+            sessionManager.AddListener(steady);
 
             this.depth = context.FindExistingNode(NodeType.Depth) as DepthGenerator;
             if (this.depth == null)
@@ -48,19 +88,86 @@ namespace SimpleViewer.net
             this.readerThread.Start();
         }
 
-        static void sessionManager_SessionStart(object sender, PositionEventArgs e)
+        void sessionManager_SessionStart(object sender, PositionEventArgs e)
         {
-            Console.WriteLine("ジェスチャーを認識する準備ができました");
+            Console.WriteLine("上下左右前後と停止のジェスチャーを認識します");
         }
 
-        static void wave_Wave(object sender, EventArgs e)
+        private void push_Push(object sender, VelocityAngleEventArgs e)
         {
-            Console.WriteLine("(-_-)/~~~ピシー!ピシー!");
+            int timeSpan = DateTime.Now.Subtract(timeStamp[ActionId.push]).Seconds;
+
+            if (timeSpan > interval)
+            {
+                Console.WriteLine("(/・ω・)/彡☆マエ");
+                timeStamp[ActionId.push] = DateTime.Now;
+            }
         }
 
-        static void push_Push(object sender, VelocityAngleEventArgs e)
+        private void push_Stable(object sender, VelocityEventArgs e)
         {
-            Console.WriteLine("(/・ω・)/彡☆");
+            int timeSpan = DateTime.Now.Subtract(timeStamp[ActionId.stable]).Seconds;
+
+            if (timeSpan > interval)
+            {
+                Console.WriteLine("|дﾟ)バックオーライ");
+                timeStamp[ActionId.stable] = DateTime.Now;
+            }
+        }
+
+        private void swipe_SwipeDown(object sender, VelocityAngleEventArgs e)
+        {
+            int timeSpan = DateTime.Now.Subtract(timeStamp[ActionId.down]).Seconds;
+
+            if (timeSpan > interval)
+            {
+                Console.WriteLine("<m(__)m>シタムキ");
+                timeStamp[ActionId.down] = DateTime.Now;
+            }
+        }
+
+        private void swipe_SwipeUp(object sender, VelocityAngleEventArgs e)
+        {
+            int timeSpan = DateTime.Now.Subtract(timeStamp[ActionId.up]).Seconds;
+
+            if (timeSpan > interval)
+            {
+                Console.WriteLine("Σ(･ω･ﾉ)ﾉウエ！");
+                timeStamp[ActionId.up] = DateTime.Now;
+            }
+        }
+
+        private void swipe_SwipeRight(object sender, VelocityAngleEventArgs e)
+        {
+            int timeSpan = DateTime.Now.Subtract(timeStamp[ActionId.right]).Seconds;
+
+            if (timeSpan > interval)
+            {
+                Console.WriteLine("(￣▽￣)(￣▽￣)ミギニオナジ");
+                timeStamp[ActionId.right] = DateTime.Now;
+            }
+        }
+
+        private void swipe_SwipeLeft(object sender, VelocityAngleEventArgs e)
+        {
+            int timeSpan = DateTime.Now.Subtract(timeStamp[ActionId.left]).Seconds;
+
+            if (timeSpan > interval)
+            {
+                Console.WriteLine("(-_-メ)ヒダリホホヲフショウ");
+                timeStamp[ActionId.left] = DateTime.Now;
+            }
+        }
+
+        private void steady_Steady(object sender, SteadyEventArgs e)
+        {
+            int timeSpan = DateTime.Now.Subtract(timeStamp[ActionId.steady]).Seconds;
+
+            if (timeSpan > interval)
+            {
+                Console.WriteLine("くコ:彡イカ！イカ！ストップ！");
+                timeStamp[ActionId.steady] = DateTime.Now;
+            }
         }
 
         protected override void OnPaint(PaintEventArgs e)
@@ -179,16 +286,5 @@ namespace SimpleViewer.net
                 this.Invalidate();
             }
         }
-
-        private readonly string SAMPLE_XML_FILE = @"../../../Data/SamplesConfig.xml";
-
-        private Context context;
-        private ScriptNode scriptNode;
-        private DepthGenerator depth;
-        private Thread readerThread;
-        private bool shouldRun;
-        private Bitmap bitmap;
-        private int[] histogram;
-        private SessionManager sessionManager;
     }
 }
